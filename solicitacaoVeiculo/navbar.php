09dce4a99b2007c55faff18a0ef2234d350cc0dc
Julia <juliahelenalimadesantana@gmail.com>
2022-11-20 14:28:23 -0300

    Adicionado projeto

---
+++ solicitacaoVeiculo/navbar.php
@@ -0,0 +1,24 @@
+<nav>
+    <div class="logo">
+        <img class="logotop" src="../templates/img/logo-tramoengenharia.png">
+    </div>
+    <div class="nav-links nav-active">
+        <a href="../../modulos/restrit/protect.php" class="nav-item  active" active-color="#d31a2b">Inicio</a>
+        <a href="#" class="nav-item" active-color="#d31a2b">Procedimentos</a>
+        <a href="#" class="nav-item" active-color="#d31a2b">solicitações</a>
+        <a href="#" class="nav-item" active-color="#d31a2b">Financeiro</a>
+        <div class="dropdown">
+            <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                <strong>mdo</strong>
+            </a>
+            <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+                <li><a class="dropdown-item" href="#">New project...</a></li>
+                <li><a class="dropdown-item" href="#">Settings</a></li>
+                <li><a class="dropdown-item" href="#">Profile</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="../../modulos/logout.php">sair</a></li>
+            </ul>
+        </div>
+    </div> 
+</nav>
